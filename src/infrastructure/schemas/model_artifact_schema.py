197408f85896b2582ec13f59e5070c5e20c10726
Marcelo <marcelo.san.car@hotmail.com>
2026-01-05 14:20:50 -0300

    chore(ml): define schema de metadata para artefatos de modelo
    - Padroniza campos obrigatórios de modelos treinados
    - Prepara persistência e versionamento de modelos

---
+++ src/infrastructure/schemas/model_artifact_schema.py
@@ -0,0 +1,11 @@
+# src/infrastructure/schemas/model_artifact_schema.py
+
+MODEL_METADATA_FIELDS = {
+    "model_type",
+    "asset_id",
+    "created_at",
+    "training_window",
+    "features_used",
+    "scaler_type",
+    "version",
+}
